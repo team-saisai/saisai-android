eb764f4050452a991c94b3a0bc9e31e61c1a1940
ChoiUS <chldn323@gmail.com>
2025-06-30 01:05:49 +0900

    feat: RecordScreen 추가

--- app/src/main/java/com/choius323/saisai/ui/screen/record/RecordScreen.kt
+++ app/src/main/java/com/choius323/saisai/ui/screen/record/RecordScreen.kt
@@ -1,19 +1,24 @@
 package com.choius323.saisai.ui.screen.record
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import com.choius323.saisai.ui.component.ProvideAppBar
+import com.choius323.saisai.ui.component.SaiText
+import com.choius323.saisai.ui.model.CourseDetails
 import com.choius323.saisai.ui.model.Position
 import com.choius323.saisai.ui.screen.map.MapScreen
+import com.choius323.saisai.ui.theme.SaiTheme
 
 @Composable
 fun RecordScreen(
@@ -28,9 +33,19 @@ fun RecordScreen(
                 modifier = Modifier.clickable(onClick = onBack)
             )
         })
+    val route = listOf(
+        Position(37.517145, 126.945064),
+        Position(37.515084, 127.000494),
+        Position(37.536378, 127.025900),
+        Position(37.523328, 127.088259),
+        Position(37.517145, 126.945064),
+        Position(37.515084, 127.000494),
+        Position(37.603300, 126.807988)
+    )
     RecordScreenContent(
-        route = listOf(Position(136.5, 36.5), Position(136.35, 36.2)),
-        modifier = modifier
+        route = route,
+        modifier = modifier,
+        { MapScreen(route, modifier = Modifier.fillMaxSize()) },
     )
 }
 
@@ -38,15 +53,55 @@ fun RecordScreen(
 fun RecordScreenContent(
     route: List<Position>,
     modifier: Modifier = Modifier,
+    mapContent: @Composable () -> Unit,
+    onEvent: () -> Unit = {},
 ) {
+    val sampleDetails = CourseDetails(
+        name = "여의대로 - 국회대로",
+        distance = "8.6km",
+        difficulty = "하",
+        themes = listOf("테마 1", "테마 1"),
+        challengerCount = 207,
+        completedCount = 412
+    )
     Box(modifier) {
-        MapScreen(route, modifier = Modifier.fillMaxSize())
-        Box(
-            Modifier
-                .fillMaxWidth(0.7f)
-                .align(Alignment.BottomCenter)
-        ) {
-            Text("$route")
+        mapContent()
+        RecordCourseDescription(
+            sampleDetails,
+            modifier = Modifier.align(Alignment.BottomCenter),
+            onChallengeClick = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RecordScreenContentPreview() {
+    val sampleDetails = CourseDetails(
+        name = "여의대로 - 국회대로",
+        distance = "8.6km",
+        difficulty = "하",
+        themes = listOf("테마 1", "테마 1"),
+        challengerCount = 207,
+        completedCount = 412
+    )
+
+    SaiTheme {
+        Surface {
+            RecordScreenContent(
+                route = emptyList(),
+                modifier = Modifier.fillMaxSize(),
+                mapContent = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.LightGray),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        SaiText("지도 영역")
+                    }
+                }
+            )
         }
     }
 }

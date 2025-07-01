b155d3810ccdee44d2c8cdd741e30bba596d320e
ChoiUS <chldn323@gmail.com>
2025-07-01 14:16:04 +0900

    feat: SessionManager 수정

--- app/src/main/java/com/choius323/saisai/data/account/SessionManager.kt
+++ app/src/main/java/com/choius323/saisai/data/account/SessionManager.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 object SessionManager {
@@ -23,23 +23,27 @@ object SessionManager {
 
         // DataStore의 토큰을 메모리로 불러오는 로직
         CoroutineScope(Dispatchers.IO).launch {
-            _accessToken.value = authDataStore.accessToken.first()
-            _refreshToken.value = authDataStore.refreshToken.first()
+            launch {
+                authDataStore.accessToken.collectLatest {
+                    _accessToken.emit(it)
+                }
+            }
+            launch {
+                authDataStore.refreshToken.collectLatest {
+                    _refreshToken.emit(it)
+                }
+            }
         }
     }
 
     // 로그인 성공 시 호출
     suspend fun onLoginSuccess(accessToken: String, refreshToken: String) {
         authDataStore.saveTokens(accessToken, refreshToken)
-        _accessToken.value = accessToken
-        _refreshToken.value = refreshToken
     }
 
     // 로그아웃 시 호출
     suspend fun onLogout() {
         authDataStore.clearTokens()
-        _accessToken.value = null
-        _refreshToken.value = null
     }
 }
 

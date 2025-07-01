c33382d991f81b1a025cbf3cf8ed04c840cc4b67
ChoiUS <chldn323@gmail.com>
2025-07-01 15:23:39 +0900

    feat: Login 화면의 email, password 속성 변경

--- app/src/main/java/com/choius323/saisai/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/com/choius323/saisai/ui/screen/login/LoginScreen.kt
@@ -3,11 +3,24 @@ package com.choius323.saisai.ui.screen.login
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.koinViewModel
 import org.orbitmvi.orbit.compose.collectAsState
@@ -39,15 +52,34 @@ fun LoginScreenContent(
     modifier: Modifier = Modifier,
     onEvent: (LoginUiEvent) -> Unit,
 ) {
+    var passwordVisible by remember { mutableStateOf(false) }
     Column(modifier) {
         TextField(
             uiState.email,
             onValueChange = { onEvent(LoginUiEvent.EmailChanged(it)) },
+            label = { Text("이메일") },
+            placeholder = { Text("이메일 주소를 입력해주세요") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            singleLine = true
         )
         Spacer(Modifier.height(8.dp))
         TextField(
             uiState.password,
             onValueChange = { onEvent(LoginUiEvent.PasswordChanged(it)) },
+            label = { Text("비밀번호") },
+            placeholder = { Text("비밀번호를 입력해주세요") },
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            singleLine = true,
+            trailingIcon = {
+                val image =
+                    if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                val description = if (passwordVisible) "비밀번호 숨기기" else "비밀번호 보이기"
+
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(imageVector = image, contentDescription = description)
+                }
+            }
         )
         Button({ onEvent(LoginUiEvent.LoginButtonClicked) }) {
             Text("로그인")

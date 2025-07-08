4ba08ad8b52241bbb06db5f935a3f9fbf8d22714
ChoiUS <chldn323@gmail.com>
2025-07-08 15:59:32 +0900

    feat: 권한 핸들러 구현

--- app/src/main/java/com/choius323/saisai/ui/component/HandlePermissionActions.kt
+++ app/src/main/java/com/choius323/saisai/ui/component/HandlePermissionActions.kt
@@ -4,10 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.platform.LocalContext
 import com.choius323.saisai.util.PermissionDescriptionProvider
 import com.choius323.saisai.util.getPermissionMap
@@ -20,16 +17,17 @@ import com.google.accompanist.permissions.shouldShowRationale
 @Composable
 fun HandlePermissionActions(
     permissionState: MultiplePermissionsState,
-    showPermissionDialog: MutableState<Boolean>,
+    isShowPermissionDialog: Boolean,
+    setShowPermissionDialog: (Boolean) -> Unit,
     onPermissionAllGranted: () -> Unit,
 ) {
     val permissionMap = getPermissionMap(permissionState.permissions.map { it.permission })
     val context = LocalContext.current
-    if (showPermissionDialog.value) {
+    if (isShowPermissionDialog) {
         when {
             permissionState.allPermissionsGranted -> {
-                if (showPermissionDialog.value) showPermissionDialog.value = false
                 onPermissionAllGranted()
+                setShowPermissionDialog(false)
             }
 
             else -> {
@@ -37,7 +35,7 @@ fun HandlePermissionActions(
                     permissionState,
                     permissionMap,
                     context,
-                    showPermissionDialog
+                    setShowPermissionDialog
                 )
             }
         }
@@ -50,12 +48,8 @@ private fun RevokedPermissionsDialog(
     permissionState: MultiplePermissionsState,
     permissionDescriptionProviderProviderMap: Map<String, PermissionDescriptionProvider>,
     context: Context,
-    showPermissionDialog: MutableState<Boolean>,
+    setShowPermissionDialog: (Boolean) -> Unit,
 ) {
-    val requestPermissionLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) {}
-
     permissionState.revokedPermissions.firstOrNull { it.status.isGranted.not() }?.also { perm ->
         val descriptionProvider =
             permissionDescriptionProviderProviderMap[perm.permission] ?: return@also
@@ -64,11 +58,9 @@ private fun RevokedPermissionsDialog(
         PermissionDialog(
             permissionDescriptionProvider = descriptionProvider,
             isPermanentlyDeclined = perm.status.isGranted.not() && perm.status.shouldShowRationale,
-            onDismiss = { showPermissionDialog.value = false },
+            onDismiss = { setShowPermissionDialog(false) },
             onOkClick = {
-//                permissionState.launchMultiplePermissionRequest()
-                val array = permissionState.permissions.map { it.permission }.toTypedArray()
-                requestPermissionLauncher.launch(array)
+                permissionState.launchMultiplePermissionRequest()
             },
             onGoToAppSettingsClick = {
                 val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {

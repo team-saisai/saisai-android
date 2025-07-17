b3614056c58b25df69179505d928740feb9463bc
ChoiUS <chldn323@gmail.com>
2025-07-17 22:36:15 +0900

    feat: 텍스트 행간 높이 변경

--- app/src/main/java/com/choius323/saisai/ui/component/SaiText.kt
+++ app/src/main/java/com/choius323/saisai/ui/component/SaiText.kt
@@ -2,7 +2,9 @@ package com.choius323.saisai.ui.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowForwardIos
@@ -16,8 +18,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -25,6 +29,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.choius323.saisai.ui.theme.SaiTheme
 
@@ -49,7 +54,8 @@ fun SaiText(
             lineHeightStyle = LineHeightStyle(
                 alignment = LineHeightStyle.Alignment.Center,
                 trim = LineHeightStyle.Trim.None,
-            )
+            ),
+            lineHeight = 1.em,
         )
     }
 
@@ -64,6 +70,46 @@ fun SaiText(
         overflow = overflow,
         maxLines = maxLines,
         style = adjustedStyle
+    )
+}
+
+@Composable
+fun SaiText(
+    text: AnnotatedString,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontWeight: FontWeight? = FontWeight.Medium,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    maxLines: Int = Int.MAX_VALUE,
+    style: TextStyle = LocalTextStyle.current,
+) {
+    val adjustStyle = remember {
+        TextStyle(
+            platformStyle = PlatformTextStyle(
+                includeFontPadding = false
+            ),
+            lineHeightStyle = LineHeightStyle(
+                alignment = LineHeightStyle.Alignment.Center,
+                trim = LineHeightStyle.Trim.None,
+            ),
+            lineHeight = 1.em,
+        )
+    }
+
+    Text(
+        text = text,
+        modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontWeight = fontWeight,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        maxLines = maxLines,
+        style = style.merge(adjustStyle)
     )
 }
 
@@ -85,16 +131,27 @@ fun TextRowBar(
 @Preview(showBackground = true)
 @Composable
 private fun TextRowBarPreview() {
-    val color = MaterialTheme.colorScheme.surface
     SaiTheme {
         Surface(
             Modifier.background(MaterialTheme.colorScheme.surface)
         ) {
-            SaiText(
-                "차단게시물 / 유저관리 $color",
-                Modifier.width(400.dp),
-                color = MaterialTheme.colorScheme.onSurface
-            )
+            Column(
+                Modifier
+                    .width(200.dp)
+                    .padding(4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                SaiText(
+                    "차단게시물 / 유저관리",
+                    color = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier.background(Color.Gray)
+                )
+                SaiText(
+                    buildAnnotatedString { append("텍스트 입니다.") },
+                    color = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier.background(Color.Gray)
+                )
+            }
         }
     }
 }

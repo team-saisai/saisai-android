910357d59a75a5133eee0dedfdf9e7c125bb077c
ChoiUS <chldn323@gmail.com>
2025-07-19 00:20:30 +0900

    feat: Loading Indicator 추가

---
+++ app/src/main/java/com/choius323/saisai/ui/component/FullScreenLoading.kt
@@ -0,0 +1,22 @@
+package com.choius323.saisai.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun FullScreenLoading() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(alpha = 0.5f)),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}

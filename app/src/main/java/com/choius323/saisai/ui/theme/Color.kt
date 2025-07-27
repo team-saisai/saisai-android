8fcafb7f8a9fad9548cb3b9f3b4f0719dd7f6396
ChoiUS <chldn323@gmail.com>
2025-07-28 00:53:59 +0900

    feat: SaiColor, SaiOpacity 추가

--- app/src/main/java/com/choius323/saisai/ui/theme/Color.kt
+++ app/src/main/java/com/choius323/saisai/ui/theme/Color.kt
@@ -11,3 +11,35 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val background = Color(0xFF202326)
+
+object SaiColor {
+    // Gray Scale
+    val Gray00 = Color(0xFFFFFFFF)
+    val White = Gray00
+    val Gray05 = Color(0xFFF1F4F7)
+    val Gray10 = Color(0xFFE8EBEE)
+    val Gray20 = Color(0xFFD2D5D9)
+    val Gray30 = Color(0xFFBABEC3)
+    val Gray40 = Color(0xFF9BA0A5)
+    val Gray50 = Color(0xFF878C91)
+    val Gray60 = Color(0xFF676D72)
+    val Gray70 = Color(0xFF51565B)
+    val Gray80 = Color(0xFF34383C)
+    val Gray90 = Color(0xFF1D2023)
+    val Gray100 = Color(0xFF000000)
+    val Black = Gray100
+
+    // Brand
+    val Lime = Color(0xFFC9FF66)
+    val Purple = Color(0xFF573BF5)
+    val LitePurple = Color(0xFF8069FD)
+
+    // System Colors
+    val SystemRed = Color(0xFFFF5252)
+}
+
+object SaiOpacity {
+    val Light = 0.2f
+    val Medium = 0.5f
+    val Dark = 0.8f
+}

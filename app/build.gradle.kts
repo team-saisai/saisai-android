d42e29df3fbc421dabebe3f41caa1ecb89671c4f
ChoiUS <chldn323@gmail.com>
2025-08-30 21:04:12 +0900

    build: 버전 정보 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.choius323.saisai"
         minSdk = 27
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

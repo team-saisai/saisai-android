20b6f4c4563f35b2306e48b38d0ee5869fd86f16
ChoiUS <chldn323@gmail.com>
2025-09-11 02:44:07 +0900

    build: 버전 업데이트 (1.0.11 -> 1.0.12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.choius323.saisai"
         minSdk = 27
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.0.11"
+        versionCode = 13
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
